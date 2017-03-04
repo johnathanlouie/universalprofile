747828b59ba64f27474d32ff2d9dfb8f84c23f4d
sashi <sashi_thapaliya@hotmail.com>
2017-03-04 02:08:56 -0800

    new credentials

--- src/socialmedia/facebook/fbapi.py
+++ src/socialmedia/facebook/fbapi.py
@@ -7,7 +7,7 @@ class FacebookAPI(object):
     def __init__(self):
        app_id="565706786893040"
        app_secret="73ed9c7a6bf22bde6c371199f3447ce7"
-       self.oauth_access_token = "EAACEdEose0cBAFNqm7OR18XY43ZAJRETKwtoN06iF7x2gwKbIyaHSk9nmdHzX5EE37p739rkj9sl0Mpuu0vwU8icFQML6PkZBTKdBLJmymxCBGKVLA46kl3PZCrIfAOqc0pk8sayyX8QWpokbUcVrM9V8WKRNhtDZCqaTegnzsrKlKq2qRU1lxxoXQAoW0kZD"
+       self.oauth_access_token = "EAACEdEose0cBAGiGeoe1RuFqIRZAqpnSiDbx7NZCzPh4ZB5R9ZCWQ7DetVJIsBPjk4dhdD89TGnvqFbUJPHuXHJbZBiEAtmIcWOMKZAxyOGQM2a4VPVxRlde4pZBdT1rkOHR5pZB8oeoh1MlU3THCZASZBPSzYr0sXpAHAIZAljZAhx81kBZAggY8t0pZCBwwW7nYn0ZAoZD"
 
 
    #@return list of profiles matching the query, with size up to limit
